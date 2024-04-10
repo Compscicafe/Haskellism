7636f6078c87008f48b16d35daddff1fb605e27b
nguyntinkhang <116292903+grimoiredecs@users.noreply.github.com>
2024-04-10 23:51:43 +0700

    Helao

--- 99/main.hs
+++ 99/main.hs
@@ -12,3 +12,12 @@ circumference r = 2 * pi * r
 
 desne :: Double -> Double
 desne r = pi * 23*r
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+let sidearea = 2* pi * r * h
+toparea = pi * r^2
+in sidearea +2 * toparea
+
+calBMI :: (RealFloat a) => [(a,a)] -> [a]
+calBMI xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
