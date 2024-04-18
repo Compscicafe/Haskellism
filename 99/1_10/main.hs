0ec6590dfb0b9eca08f8b0b0f0f07cde76a3b427
nguyntinkhang <116292903+grimoiredecs@users.noreply.github.com>
2024-04-18 11:30:48 +0700

    full

--- 99/1_10/main.hs
+++ 99/1_10/main.hs
@@ -7,15 +7,35 @@ in_range min max x = x>=min && x<max
 
 
 fact :: (Ord t, Num t) => t -> t
-fact n 
+fact n
     | n <=1 = 1
     | otherwise = n*fact(n-1)
 
-is_zero 0 = True
-is_zero _ = False
+
+myButLast [x,_]  = x
+myButLast (_:xs) = myButLast xs
+
+elementAt list i = list !! (i-1)
 
 
 
-head':: [Num] => Num
-    | = head
-    
+numele [] = 0
+numele (_:xs) =1+ numele(xs)
+
+--data Mag = Mag String Int [String] deriving (Show)
+encode :: Eq a => [a] -> [(Int,a)]
+encode [] = []
+encode (x: xs) = (length ( x : takeWhile(==x) xs), x): encode (dropWhile(==x) xs)
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x: xs) = (x: takeWhile(==x) xs) : pack(dropWhile(==x) xs)
+
+compres [] = []
+compres (x: xs) = x: compres(dropWhile(==x) xs)
+--recursive solution
+
+compres_ (x:ys@(y:_))
+    |x==y = compres_ ys
+    |otherwise = x : compres_ ys
+compres_ ys = ys
--- 99/main.hs
+++ 99/main.hs
@@ -15,9 +15,55 @@ desne r = pi * 23*r
 
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
-let sidearea = 2* pi * r * h
-toparea = pi * r^2
-in sidearea +2 * toparea
+    let sidearea = 2* pi * r * h
+        toparea = pi * r^2
+    in sidearea +2 * toparea
 
 calBMI :: (RealFloat a) => [(a,a)] -> [a]
 calBMI xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+
+maximum_ :: (Ord a) => [a]->a
+maximum_ [] = error "NO MOREs"
+maximum_ [x] = x
+maximum_ (x:xs)
+    | x > maxtail = x
+    | otherwise = maxtail
+    where maxtail = maximum_ xs
+
+replicate_ :: (Num i, Ord i) => i -> a -> [a]
+replicate_ n x
+    | n <= 0 = []
+    | otherwise = x:replicate_ (n-1) x
+
+linearSearch :: (Int -> Int) -> Int -> [Int]
+linearSearch f t = [x| x <- [0..t], t == f x]
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x*z*y
+
+ifEvenAdd2 :: Integer -> Maybe Integer
+ifEvenAdd2 n = if even n then Just (n+2 ) else Nothing
+
+
+ifOddMinus3 :: Integer -> Maybe Integer
+ifOddMinus3 n = if odd n then Just (n-3) else Nothing
+
+
+
+
+--palindrome(*) Find out whether a list is a palindrome.
+palindrome xs = xs == reverse xs
+
+data NestedList a = Elem a | List [NestedList a]
+
+--flatten(*) Flatten a nested list structure.
+flatten :: NestedList a -> [a]
+
+flatten (Elem x) = [x]
+flatten (List x) = concatMap flatten x
+
+-- comrpess
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress (x:xs) = x:compress (dropWhile (==x) xs)
